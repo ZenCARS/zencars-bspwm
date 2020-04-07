4e66c7015afce2199436e1d7a4818d4fddc62746
ZengineerIO <zengineer@endocluster.com>
2020-04-07 17:55:03 +0200

    reenabled autostart for welcome-app

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -33,7 +33,7 @@ feh --bg-scale ~/.config/wall.png &
 #feh --randomize --bg-fill ~/Képek/*
 #feh --randomize --bg-fill ~/Dropbox/Apps/Desktoppr/*
 
-# dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
+dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
 xsetroot -cursor_name left_ptr &
 run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
 
